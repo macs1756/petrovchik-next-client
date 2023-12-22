28f84b4f19143fc6b5d355ae3f6fc4b1d48469c2
macs1756 <kindrativmaksum312@gmail.com>
2023-12-22 21:52:06 +0200

    create some url links

---
+++ src/app/utils/links.ts
@@ -0,0 +1,13 @@
+
+const domain = 'http://localhost:3000/'
+const current_language = ''
+
+export const HOME_PAGE = domain + '/'
+export const WORK_CATALOG = domain + '/work/'
+export const DWELLING_CATALOG = domain + '/dwelling/'
+export const BUSINESS_CATALOG = domain + '/business/'
+export const BLOG_CATALOG = domain + '/blog/'
+export const SEARCH = domain + '/search/'
+export const CREATE_WORK = domain + 'form/work/'
+export const CREATE_DWELLING = domain + 'form/dwelling/'
+export const CREATE_BUSINESS = domain + 'form/business/'
