e05b94435d74c3b520d062b96be895c36fec7a31
macs1756 <kindrativmaksum312@gmail.com>
2023-12-24 17:31:58 +0200

    modal v2

--- src/components/HeaderModalForCreatedNewElement/index.tsx
+++ src/components/HeaderModalForCreatedNewElement/index.tsx
@@ -3,9 +3,9 @@ export default function HeaderModalForCreatedNewElement() {
     <div className="h-full">
       <button className="tr bg-yellow h-full py-[14px] px-[30px] text-[16px] rounded-[6px] hover:bg-yellow-hover">Додати оголошення</button>
 
-      <div className="modal__wr">
+      <div className="modal__wr active">
         <div className="modal__body">
-          <h2 className="text-[40px] mb-[40px]">Додати оголошення</h2>
+          <h2 className="text-[40px] mb-[40px] text-center">Додати оголошення</h2>
 
           <div className="grid gap-[24px] grid-cols-3">
               test
